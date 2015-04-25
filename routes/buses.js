d0ad43ffbec6b589aa5e83ca831183bf2eae9536
Alan <alan220193@hotmail.com>
2015-04-25 03:28:39 -0600

    ver1

--- routes/buses.js
+++ routes/buses.js
@@ -2,3 +2,15 @@
 exports.buses = function(req, res){
   res.render('buses', { title: 'Buses' });
 };
+
+var express        =         require("express");
+var bodyParser     =         require("body-parser");
+var app            =         express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/buses',function(req,res){
+  var user_name=req.body.placa;
+  var password=req.body.modelo;
+  console.log("User name = "+user_name+", password is "+password);
+  res.end("yes");
+});
